79e60b2ef1e175377481602f67a4808d378598ec
Alexkim <ghg303@naver.com>
2023-05-19 16:30:31 +0900

    스켈레톤코드구현__Review*__리뷰페이지 스켈레톤 코드 구현, 컴포넌트 간 연결

---
+++ final_pjt_front/src/components/Review/ReviewList.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="ReviewList">
+    <div style="border: solid black">
+      <h1>리뷰 게시판</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ReviewList",
+  components: {},
+}
+</script>
+
+<style></style>
--- final_pjt_front/src/views/ReviewView.vue
+++ final_pjt_front/src/views/ReviewView.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="ReviewView">
     <h1>Review Page</h1>
+    <ReviewList />
   </div>
 </template>
 
 <script>
+import ReviewList from "@/components/Review/ReviewList"
 export default {
   name: "ReviewView",
-  components: {},
+  components: {
+    ReviewList,
+  },
 }
 </script>
