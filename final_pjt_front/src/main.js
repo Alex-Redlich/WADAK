adf82ac34891f8832ba4545bb2a652bb62c603f0
Alex-kim <ghg303@naver.com>
2023-05-19 23:07:38 +0900

    Hotfix__main.js__부트스트랩 수정 완료, 데모버전 확인

--- final_pjt_front/src/main.js
+++ final_pjt_front/src/main.js
@@ -1,14 +1,23 @@
-import Vue from "vue"
-import App from "./App.vue"
-import router from "./router"
-import { BootstrapVue, IconsPlugin } from "bootstrap-vue"
-import "bootstrap/dist/css/bootstrap.css"
-import "bootstrap-vue/dist/bootstrap-vue.css"
+import "@babel/polyfill";
+import "mutationobserver-shim";
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+// import store from "./store";
 
-Vue.use(BootstrapVue)
-Vue.use(IconsPlugin)
+import { BootstrapVue, BootstrapVueIcons } from "bootstrap-vue";
+import "bootstrap-vue/dist/bootstrap-vue-icons.min.css";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+Vue.use(BootstrapVue);
+Vue.use(BootstrapVueIcons);
+
+Vue.config.productionTip = false;
+
 new Vue({
   router,
-
+  // store,
   render: (h) => h(App),
-}).$mount("#app")
+}).$mount("#app");
