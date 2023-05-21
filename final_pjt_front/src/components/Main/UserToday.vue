8b9b289eec5e7050fa242d02e08f20262c2509be
Alex-kim <ghg303@naver.com>
2023-05-22 00:16:38 +0900

    페이지구현__MovieCardOneDetail.vue,UserToday.vue__유저 투데이무비 페이지 구현

--- final_pjt_front/src/components/Main/UserToday.vue
+++ final_pjt_front/src/components/Main/UserToday.vue
@@ -1,21 +1,36 @@
 <template>
   <div class="UserToday">
-    <div style="border: solid black">
-      <h1>유저의 오늘의 영화</h1>
+    <!-- 유저 이름 태그 블럭 -->
+    <div class="d-flex justify-content-start align-items-baseline ps-3 pl-3">
+      <p id="UserReviewlist_username">세종시 속도제한 20km</p>
+      <p id="UserReviewlist_text">님의 Today Movie</p>
+    </div>
+    <!-- 단독 카드 -->
+    <div class="d-flex justify-content-center">
       <MovieCardOneDetail />
     </div>
   </div>
 </template>
 
 <script>
-import MovieCardOneDetail from "@/components/Main/MovieCardOneDetail"
+import MovieCardOneDetail from "@/components/Main/MovieCardOneDetail";
 
 export default {
   name: "UserToday",
   components: {
     MovieCardOneDetail,
   },
-}
+};
 </script>
 
-<style></style>
+<style>
+#UserReviewlist_username {
+  font-size: 50px;
+  font-weight: 800;
+  margin-right: 10px;
+}
+#UserReviewlist_text {
+  font-size: 25px;
+  font-weight: 300;
+}
+</style>
