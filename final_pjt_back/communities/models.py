f534b873da4673917e3dd8f0c7d88e6997d43001
김효인 <bright_bat@naver.com>
2023-05-22 14:01:00 +0900

    communitiesApp__models.py serailizers.py views.py urls.py__Review, Comment CRUD

--- final_pjt_back/communities/models.py
+++ final_pjt_back/communities/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.conf import settings
+from ..movies.models import Movie
+
+class Review(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name="reviews")
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="reviews")
+    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="like_reviews")
+    
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    rating = models.IntegerField()
+
+class Comment(models.Model):
+    review = models.ForeignKey(Review, on_delete=models.CASCADE, related_name="comments")
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="comments")
+    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="like_comments")
+    # parent_comment = models.ForeignKey('self', on_delete=models.CASCADE, related_name="recomments", null= True)
+    # 대댓글
+    
+    content = models.CharField(max_length=50)
 
 # Create your models here.
