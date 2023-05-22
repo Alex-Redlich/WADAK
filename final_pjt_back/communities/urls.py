e2f99daa5448fb81ba995bde9a648e240acffba0
김효인 <bright_bat@naver.com>
2023-05-22 14:06:14 +0900

    communitiesApp__urls.py__url 수정

--- final_pjt_back/communities/urls.py
+++ final_pjt_back/communities/urls.py
@@ -1,6 +1,16 @@
 from django.urls import path
+from . import views
 
 app_name = 'communities'
 urlpatterns = [
+    path('movie/<int:movie_pk>/review/',views.review_list),
+    path('movie/<int:movie_pk>/review/create/',views.review_create),
+    path('movie/<int:movie_pk>/review/<int:review_pk>/',views.review_detail),
+    path('movie/<int:movie_pk>/review/<int:review_pk>/update/',views.review_update),
+    path('movie/<int:movie_pk>/review/<int:review_pk>/delete/',views.review_delete),
     
+    path('review/<int:review_pk>/comment/',views.comment_list),
+    path('review/<int:review_pk>/comment/create/',views.comment_create),
+    path('review/<int:review_pk>/comment/<int:comment_pk>/update/',views.comment_update),
+    path('review/<int:review_pk>/comment/<int:comment_pk>/delete/',views.comment_delete),
 ]
