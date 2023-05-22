3aaf206a43a8f2e5d70bdaec77ddb2a2b55c3368
Alex <ghg303@naver.com>
2023-05-22 21:53:40 +0900

    페이지구현중__*__유저프로필페이지작업중

--- final_pjt_front/src/components/Profile/UserInfo.vue
+++ final_pjt_front/src/components/Profile/UserInfo.vue
@@ -1,7 +1,24 @@
 <template>
   <div class="UserInfo">
-    <div style="border: solid black">
-      <h1>유저 정보</h1>
+    <div id="UserInfo">
+      <div class="row">
+        <dlv id="UserImagediv" class="col-4">
+          <img id="UserImage" src="@/assets/profile.jpg" alt="" />
+          <button id="EditBtn" class="btn btn-warning">Edit Profile</button>
+        </dlv>
+        <div id="UserDetail" class="col-4">
+          <p>칭호</p>
+          <p>닉네임</p>
+          <p>아 하기싫어..아 하기싫어..</p>
+        </div>
+        <div id="Userintro" class="col-4">
+          <p>팔로잉 : 0 명</p>
+          <p>팔로워 : 0 명</p>
+          <p>내 게시글 : 0 명</p>
+          <p>내 코멘트 : 0 명</p>
+          <div class="expoint">경험치바</div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -13,4 +30,43 @@ export default {
 }
 </script>
 
-<style></style>
+<style>
+.UserInfo {
+  display: flex;
+  justify-content: center;
+}
+#UserImagediv {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+#UserImage {
+  border-radius: 3000px;
+  width: 300px;
+  height: 300px;
+  margin: 25px;
+}
+#EditBtn {
+  width: 200px;
+  border-radius: 30px;
+  margin-bottom: 20px;
+}
+#UserInfo {
+  background-color: white;
+  width: 1500px;
+  color: black;
+  border-radius: 30px;
+}
+#UserDetail {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+#Userintro {
+  margin-top: 30px;
+}
+.expoint {
+  height: 100px;
+  background-color: blueviolet;
+}
+</style>
