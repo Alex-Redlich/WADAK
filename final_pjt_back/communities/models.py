4e08acdcb33090cb6e5077f285fcc037e72250ab
김효인 <bright_bat@naver.com>
2023-05-22 17:22:33 +0900

    communitiesApp__models.py__Movie import 수정

--- final_pjt_back/communities/models.py
+++ final_pjt_back/communities/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.conf import settings
-from ..movies.models import Movie
+from movies.models import Movie
 
 class Review(models.Model):
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name="reviews")
