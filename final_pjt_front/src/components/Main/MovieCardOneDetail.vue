51f78cc3983b2b70e39dd58f0fba7dac4f6ae24e
Alex-kim <ghg303@naver.com>
2023-05-24 23:06:47 +0900

    페이지구현__UserToday.vue__유저 투데이 무비 추천알고리즘 페이지 구현

--- final_pjt_front/src/components/Main/MovieCardOneDetail.vue
+++ final_pjt_front/src/components/Main/MovieCardOneDetail.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="MovieCardOneDetail1">
-    <div id="MovieCardOneDetail1" class="card" style="max-width: 1200px">
+    <div id="MovieCardOneDetail1" class="card hovering5" style="max-width: 1300px" @click="GoDetail">
       <div class="row">
         <div class="col-4">
-          <img src="https://image.tmdb.org/t/p/w500/3JT1oE0JIA6CQJaZDG00y3S0kP3.jpg" class="img-fluid rounded-start" />
+          <img :src="url" class="img-fluid rounded-start" />
         </div>
         <div class="col-8 p-5">
           <div class="card-body">
-            <h5 class="card-title">분노의 질주: 라이드 오어 다이</h5>
-            <p class="card-text">2023-05-17</p>
-            <p class="card-text">⭐ 7.1</p>
+            <h5 class="card-title">{{ movie.title }}</h5>
+            <p class="card-text">{{ movie.release_date }}</p>
+            <p class="card-text">⭐ {{ movie.vote_average }}</p>
             <p class="card-text-detail">
-              돔과 그의 패밀리 앞에 나타난 운명의 적 단테. 과거의 그림자는 돔의 모든 것을 파괴하기 위해 달려온다. 단테에 의해 산산히 흩어진 패밀리들은 모두 목숨을 걸고 맞서야 하는 함정에 빠지고
-              마는데...
+              {{ movie.overview }}
             </p>
           </div>
         </div>
@@ -24,7 +23,23 @@
 <script>
 export default {
   name: "MovieCardOneDetail",
-}
+  props: {
+    movie: Object,
+  },
+  date() {
+    return {};
+  },
+  computed: {
+    url() {
+      return "https://image.tmdb.org/t/p/w500" + this.movie.poster_path;
+    },
+  },
+  methods: {
+    GoDetail() {
+      this.$router.push({ name: "moviedetail", params: { moviePK: this.movie.id } });
+    },
+  },
+};
 </script>
 
 <style>
@@ -46,5 +61,11 @@ export default {
 }
 #MovieCardOneDetail1 {
   box-shadow: 3px 3px 5px rgb(212, 212, 212);
+}
+.hovering5:hover {
+  -webkit-filter: grayscale(100%) blur(2px);
+  filter: grayscale(50%) blur(2px);
+  -webkit-transform: scale(1.1);
+  transform: scale(1.1);
 }
 </style>
--- final_pjt_front/src/components/Main/UserToday.vue
+++ final_pjt_front/src/components/Main/UserToday.vue
@@ -2,23 +2,47 @@
   <div class="UserToday mb-4">
     <!-- 유저 이름 태그 블럭 -->
     <div class="d-flex justify-content-start align-items-baseline ps-3 pl-3">
-      <p id="UserReviewlist_username">세종시 속도제한 20km</p>
+      <p id="UserReviewlist_username">{{ following_nickname }}</p>
       <p id="UserReviewlist_text">님의 Today Movie</p>
     </div>
     <!-- 단독 카드 -->
     <div class="d-flex justify-content-center">
-      <MovieCardOneDetail />
+      <MovieCardOneDetail :movie="usertoday_movie" />
     </div>
   </div>
 </template>
 
 <script>
 import MovieCardOneDetail from "@/components/Main/MovieCardOneDetail";
+import axios from "axios";
 
 export default {
   name: "UserToday",
   components: {
     MovieCardOneDetail,
+  },
+  data() {
+    return {
+      usertoday_movie: {},
+      following_nickname: "",
+    };
+  },
+  methods: {
+    getUserToday() {
+      axios({
+        method: "get",
+        url: `http://127.0.0.1:8000/api/v1/movies/8/follow/today/`,
+      })
+        .then((res) => {
+          console.log(res.data);
+          this.usertoday_movie = res.data;
+          this.following_nickname = res.data.following_nickname;
+        })
+        .catch((err) => console.log(err));
+    },
+  },
+  created() {
+    this.getUserToday();
   },
 };
 </script>
