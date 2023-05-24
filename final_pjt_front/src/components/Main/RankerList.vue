ad719a0e73bebf79b4d5299713d5c085002e9851
Alex-kim <ghg303@naver.com>
2023-05-25 00:52:09 +0900

    Hotfix__Ranker.vue__경로 오류 수정

--- final_pjt_front/src/components/Main/RankerList.vue
+++ final_pjt_front/src/components/Main/RankerList.vue
@@ -10,10 +10,10 @@
     <!-- 랭커 표시-->
     <div class="RankerSection">
       <div data-aos="fade-right" data-aos-duration="1000" data-aos-delay="500">
-        <p class="m-0" style="padding-left: 30px">{{ ranker_movies.chingho }}</p>
+        <p class="m-0" style="padding-left: 30px">{{ userInfo.chingho }}</p>
         <div class="d-flex align-items-baseline">
           <p style="font-size: 7rem; text-shadow: 5px 5px 15px black; font-weight: 1000" @click="GoProfile">
-            {{ ranker_movies.ranker_nickname }}
+            {{ userInfo.ranker_nickname }}
           </p>
           <p style="font-size: 2rem">의 Ranker Pick</p>
         </div>
@@ -44,7 +44,7 @@ export default {
   data() {
     return {
       ranker_movies: {},
-      userID_ranker: "",
+      userInfo: {},
     };
   },
   methods: {
@@ -62,8 +62,8 @@ export default {
       })
         .then((res) => {
           console.log(res.data);
-          this.ranker_movies = res.data;
-          this.userID_ranker = res.data.id;
+          this.ranker_movies = res.data.movies;
+          this.userInfo = res.data;
         })
         .catch((err) => console.log(err));
     },
